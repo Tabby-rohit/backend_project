5aff73ea78be4489dd4784ec8b2b9b8f04f6a08c
Tabby-rohit <dayanandbellikatti@gmail.com>
2026-03-31 18:38:45 +0530

    feat: add user account management routes for password change, profile updates, and watch history retrieval

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser, loginUser, logoutUser } from "../controllers/user.controller.js";
+import { registerUser, loginUser, logoutUser, updatecurrentUser,updatecurrentUserCoverImage,updatecurrentUserAvatar,updatecurrentUser,changeCurrentPassword,getUserChannelProfile,getUserWatchHistory,getUserChannelProfile,getcurrentUser,refreshaccessToken } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router();
@@ -14,4 +14,11 @@ router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJWT, logoutUser); 
 // is me jab hum 2 func denge to router confuse ho jata kei konsa pahle run karu to isliye hum kop next() dalte hai taki wo dono func run kar sake aur jab bhi hum logout route ko hit kare to pehle verifyJWT middleware run hoga aur uske baad logoutUser function run hoga
 router.route("/refresh-token").post(refreshaccessToken)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/update-account").patch(verifyJWT,updatecurrentUser)
+router.route("/current-user").get(verifyJWT, getcurrentUser)
+router.route("/avatar").patch(verifyJWT,upload.single('avatar'), updatecurrentUserAvatar)
+router.route("/cover-image").patch(verifyJWT,upload.single('coverImage'), updatecurrentUserCoverImage)
+router.route("/c/:username/channel-profile").get(verifyJWT, getUserChannelProfile)
+router.route("/history").get(verifyJWT, getUserWatchHistory)
 export default router;

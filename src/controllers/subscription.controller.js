483890486a9251fe98a26c9395b4c95a0d0d32c0
Tabby-rohit <dayanandbellikatti@gmail.com>
2026-04-05 12:06:31 +0530

    feat: enhance subscription controller with unsubscribe and isSubscribed functionalities

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -3,6 +3,7 @@ import { User } from "../models/user.model";
 import { Subscription } from "../models/subscription.model";
 import { apierror } from "../utils/apiError";
 import { request } from "express";
+import { get } from "mongoose";
 // import { Apiresponse } from "../utils/apiResponse";
 
 const subscribe=asyncHandler(async(req,res)=>{
@@ -20,6 +21,43 @@ const subscribe=asyncHandler(async(req,res)=>{
       apierror("already subscribed",400)
    }
    const newSubscription=await Subscription.create({subscriber:subscriber._id,channel:channel._id})
-   return res.status(200).json(new Apiresponse(200,true,"subscribed successfully",newSubscription) )
+   return res.status(200).json(new Apiresponse("subscribed successfully",200) )
    })
+const unsubscribe=asyncHandler(async(req,res)=>{
+    const subscriber=await User.findByUserId(req._id)
+      if(!subscriber){
+         apierror("user not logged in",404)
+      }
+   const id=req.body._id
+   if(!id){
+    new apierror("channel not found",404)  
+   }        
+    const channel=await User.findByUserId(id)
+    const subscription=await Subscription.findOne({subscriber:subscriber._id,channel:channel._id})
+      if(!subscription){
+         apierror("not subscribed",400)
+      }
+      await subscription.deleteOne()
+      return res.status(200).json(new Apiresponse("unsubscribed successfully",200) )
+   
+   })
+const isSubscribed=asyncHandler(async(req,res)=>{
+      const subscriber=await User.findByUserId(req._id)
+      if(!subscriber){
+         apierror("user not logged in",404)
+      }
+      const channelId=req.params.channelId
+      const channel=await User.findByUserId(channelId)
+      if(!channel){
+         apierror("channel not found",404)
+      }
+      const subscription=await Subscription.findOne({subscriber:subscriber._id,channel:channel._id})
+      if(!subscription){
+         return res.status(200).json(new Apiresponse("not subscribed",400))
+      }
+      return res.status(200).json(new Apiresponse("subscribed",200))
+
+   })
+
+export {subscribe,unsubscribe,isSubscribed}
 

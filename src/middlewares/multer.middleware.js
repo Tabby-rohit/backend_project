0ba2c27ba318794959e20e603848e89fc99fc540
Tabby-rohit <dayanandbellikatti@gmail.com>
2026-04-18 14:46:49 +0530

    fix: switch multer storage from disk to memory

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,16 +1,6 @@
 
 import multer from "multer";
 
-const storage = multer.diskStorage({
-    destination: function (req, File, cb) {
-      cb(null, "./public/temp")
-    },
-    filename: function (req, File, cb) {
-      const name = Date.now() + "_" + File.originalname.replace(/\s+/g, "_");
-      cb(null, name)
-    }
-  })
-  
-export const upload = multer({ 
-    storage, 
-})
+const storage = multer.memoryStorage();
+
+export const upload = multer({ storage });

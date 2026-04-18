56fedab4c9390df4f012b8f66f4e0071bcefdffb
Tabby-rohit <dayanandbellikatti@gmail.com>
2026-04-18 11:42:21 +0530

    feat: restructure project by moving index.html to root and updating vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  root: '..',
+  publicDir: 'frontend/public',
   plugins: [react()],
+  build: {
+    outDir: 'frontend/dist',
+    emptyOutDir: true,
+  },
 })

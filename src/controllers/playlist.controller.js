63e5b214660c3a78b6fb643062f80c0f0d8efa84
Tabby-rohit <dayanandbellikatti@gmail.com>
2026-04-18 14:11:20 +0530

    Fix import case for ApiError in playlist.controller.js

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -1,7 +1,7 @@
 import {isValidObjectId} from "mongoose"
 import {Playlist} from "../models/playlist.model.js"
 import {Video} from "../models/video.model.js"
-import {ApiError} from "../utils/apiError.js"
+import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
